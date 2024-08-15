611caefa937d275687bad3360a0622781e384adf
fedallah-jr <kilciali2728@gmail.com>
2024-08-16 01:57:15 +0300

    Update data_loader.py

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -236,35 +236,40 @@ class Dataset_Custom(Dataset):
 
     def __read_data__(self):
         self.scaler = StandardScaler()
-    
-        # Load the CSV data
-        df_raw = pd.read_csv(os.path.join(self.root_path, self.data_path))
-    
-        # Determine the start and end of the dataset based on the flag (train, val, test)
-        border1s = [0, 12 * 30 * 24 - self.seq_len, 12 * 30 * 24 + 4 * 30 * 24 - self.seq_len]
-        border2s = [12 * 30 * 24, 12 * 30 * 24 + 4 * 30 * 24, 12 * 30 * 24 + 8 * 30 * 24]
+        df_raw = pd.read_csv(os.path.join(self.root_path,
+                                          self.data_path))
+
+        '''
+        df_raw.columns: ['date', ...(other features), target feature]
+        '''
+        cols = list(df_raw.columns)
+        cols.remove(self.target)
+        cols.remove('date')
+        df_raw = df_raw[['date'] + cols + [self.target]]
+        num_train = int(len(df_raw) * 0.7)
+        num_test = int(len(df_raw) * 0.2)
+        num_vali = len(df_raw) - num_train - num_test
+        border1s = [0, num_train - self.seq_len, len(df_raw) - num_test - self.seq_len]
+        border2s = [num_train, num_train + num_vali, len(df_raw)]
         border1 = border1s[self.set_type]
         border2 = border2s[self.set_type]
-    
-        # Handle different feature modes
+
         if self.features == 'M' or self.features == 'MS':
-            cols_data = df_raw.columns[1:]  # Exclude 'date'
+            cols_data = df_raw.columns[1:]
             df_data = df_raw[cols_data]
         elif self.features == 'S':
             df_data = df_raw[[self.target]]
-        elif self.features == 'MS_exclude_target':  # New mode
-            cols_data = df_raw.columns[1:]  # Exclude 'date'
-            df_data = df_raw[cols_data].drop([self.target], axis=1)  # Exclude target
-    
-        # Apply scaling if enabled
+        elif self.features == 'MS_exclude_target':
+            cols_data = df_raw.columns[1:-1]  # exclude target feature
+            df_data = df_raw[cols_data]
+
         if self.scale:
             train_data = df_data[border1s[0]:border2s[0]]
             self.scaler.fit(train_data.values)
             data = self.scaler.transform(df_data.values)
         else:
             data = df_data.values
-    
-        # Handle time encoding
+
         df_stamp = df_raw[['date']][border1:border2]
         df_stamp['date'] = pd.to_datetime(df_stamp.date)
         if self.timeenc == 0:
@@ -276,33 +281,27 @@ class Dataset_Custom(Dataset):
         elif self.timeenc == 1:
             data_stamp = time_features(pd.to_datetime(df_stamp['date'].values), freq=self.freq)
             data_stamp = data_stamp.transpose(1, 0)
-    
-        # Assign the processed data
+
         self.data_x = data[border1:border2]
-        self.data_y = data[border1:border2]
-    
+        self.data_y = df_raw[[self.target]].values[border1:border2]  # always use target feature as output
+
+        if self.set_type == 0 and self.args.augmentation_ratio > 0:
+            self.data_x, self.data_y, augmentation_tags = run_augmentation_single(self.data_x, self.data_y, self.args)
+
         self.data_stamp = data_stamp
 
-
     def __getitem__(self, index):
-        # Calculate sequence boundaries
         s_begin = index
         s_end = s_begin + self.seq_len
         r_begin = s_end - self.label_len
         r_end = r_begin + self.label_len + self.pred_len
-    
-        # Input sequence without the target feature (this is always true if MS_exclude_target was handled during __read_data__)
+
         seq_x = self.data_x[s_begin:s_end]
-    
-        # Target sequence (only the target feature)
         seq_y = self.data_y[r_begin:r_end]
-    
-        # Time mark sequences
         seq_x_mark = self.data_stamp[s_begin:s_end]
         seq_y_mark = self.data_stamp[r_begin:r_end]
-    
-        return seq_x, seq_y, seq_x_mark, seq_y_mark
 
+        return seq_x, seq_y, seq_x_mark, seq_y_mark
 
     def __len__(self):
         return len(self.data_x) - self.seq_len - self.pred_len + 1
